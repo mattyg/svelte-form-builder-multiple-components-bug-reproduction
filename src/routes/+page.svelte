18685c3a6ed80380a7d7a1b9515ece7852faa418
Matt Gabrenya <matt@buildyourweb.app>
2024-02-27 09:14:50 -0700

    reproduction

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { FormBuilder } from "@pragmatic-engineering/svelte-form-builder-community";
+
+</script>
+
+<FormBuilder
+	options={{
+		view: "build",
+	}} 
+/>
+
+<FormBuilder
+	options={{
+		view: "build",
+	}}
+/>
---
+++ src/routes/working/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { FormBuilder } from "@pragmatic-engineering/svelte-form-builder-community";
+
+</script>
+
+<FormBuilder
+	options={{
+		view: "build",
+	}}
+/>
